82b5fb07d04cf4fdc397fab2738ba8be20b39644
AlvinAttalie-id <alvin.attalie31@gmail.com>
2025-06-22 23:21:44 +0800

    update Welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,6 +12,7 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <link rel="icon" href="{{ asset('logo-cusomize.png') }}">
 
     <!-- Styles (Tailwind + custom) & Scripts via Vite -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
